e672e30a6404164cafe6e13399874e53e68185aa
weiss-ben <90002567+weiss-ben@users.noreply.github.com>
2022-04-22 23:09:28 +0300

    Update exercise_2_3.cpp

--- chapter2/exercise_2_3.cpp
+++ chapter2/exercise_2_3.cpp
@@ -4,44 +4,56 @@ using std::cin;
 
 int main()
 {
+    short len = 12;
     short space = 0;
-    short pound = 2;
-    short length = 8;
-    bool ascending = true;
 
-    while(length > 0)
+    while(len > 0)
     {
-        for(int i = 0; i < space; ++i)
+        //Left space
+        for(int j = 0; j < space; ++j)
         {
             cout << ' ';
         }
-        for(int j = 0; j < pound / 2; ++ j)
+        //Left pound
+        for(int j = 0; j < space + 1; ++j)
         {
             cout << '#';
         }
-
-        //Check for order
-        if(ascending)
+        //Center spaces
+        for(int i = len; i > space; --i)
         {
-            ++space;
-            pound += 2;
+            cout << ' ';
         }
-        else
+        //Right pound
+        for(int j = 0; j < space + 1; ++j)
         {
-            --space;
-            pound -= 2;
+            cout << '#';
         }
-
-        //Switch order
-        if(pound == 10)
-        {
-            ascending = false;
-
-            pound = 8;
-            space = 3;
-        }
-
-        --length;
         cout << '\n';
+        len -=3;
+        ++space;
+    }
+
+    while(len <= 12)
+    {
+        for(int i = space - 1; i > 0; --i)
+        {
+            cout << ' ';
+        }
+        for(int j = 0; j < space; ++j)
+        {
+            cout << '#';
+        }
+        for(int i = space; i < len + 4; ++i)
+        {
+            cout << ' ';
+        }
+        for(int j = 0; j < space; ++j)
+        {
+            cout << '#';
+        }
+        cout << '\n';
+        len +=3;
+        --space;
     }
 }
